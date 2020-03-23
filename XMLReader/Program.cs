c0f202bf34b9ece51c1afd74d760ed1ce955898a
Conterry <glebois05@gmail.com>
2020-03-23 20:17:54 +0200

    WIP

---
+++ XMLReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace XMLReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Reader();
+        }
+
+        static void Reader()
+        {
+            StreamReader sr = new StreamReader("xml.txt");
+            while (!sr.EndOfStream)
+            {
+                string a = sr.ReadLine();
+                Console.WriteLine(a);
+            }
+            
+        }
+
+    }
+}
